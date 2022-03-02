f1784421aadb21d5c436d308b228f0bb9ccb8720
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-03-02 19:33:19 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,10 @@
+fun main (){
+    myResult(34,54)
+}
+fun myResult(num1:Int, num2:Int):Int{
+    var  num1 = 34
+    var num2 = 54
+    var product = 0.5.toInt()*num1*num2
+    println(product)
+    return product
+}
